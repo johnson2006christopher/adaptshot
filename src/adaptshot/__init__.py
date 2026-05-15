5090f529ba8ca23bd65bbddc6ede21c9cf164f5e
johnson2006christopher <johnson2006christopher@gmail.com>
2026-05-15 06:37:46 +0300

    feat: initialize AdaptShot module with version and core components

---
+++ src/adaptshot/__init__.py
@@ -0,0 +1,19 @@
+"""AdaptShot: Human-Aligned Few-Shot Vision Learning."""
+
+__version__ = "0.1.0"
+
+from .config.settings import AdaptShotConfig
+from .core.learner import FewShotLearner
+from .core.calibration import CalibrationEngine
+from .core.act import ACTEngine
+from .training.feedback_router import FeedbackRouter
+from .training.up_ugf import UPUGFPruner
+
+__all__ = [
+    "AdaptShotConfig",
+    "FewShotLearner",
+    "CalibrationEngine",
+    "ACTEngine",
+    "FeedbackRouter",
+    "UPUGFPruner",
+]
